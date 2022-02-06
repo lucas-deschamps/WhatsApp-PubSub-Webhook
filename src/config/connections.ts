13c0afccbe1dedab909ce73e87565b35e6bf2eed
Lucas Deschamps <l-deschamps@outlook.com>
2022-02-06 19:49:02 -0300

    updating environment variable name

--- src/config/connections.ts
+++ src/config/connections.ts
@@ -2,7 +2,7 @@ import { config } from "dotenv";
 
 config();
 
-export const amqpServer = `amqp://${process.env.RABBITMQ_USR}:${process.env.RABBITMQ_PSS}@${process.env.RABBITMQ_URI}:${process.env.RABBITMQ_PORT}`;
+export const amqpServer = `amqp://${process.env.RABBITMQ_USR}:${process.env.RABBITMQ_PSS}@${process.env.RABBITMQ_HOST}:${process.env.RABBITMQ_PORT}`;
 
-export const amqpDevServer = `amqp://${process.env.RABBITMQ_DEV_USR}:${process.env.RABBITMQ_DEV_PSS}@${process.env.RABBITMQ_DEV_URI}:${process.env.RABBITMQ_DEV_PORT}`;
-export const amqpPrdServer = `amqp://${process.env.RABBITMQ_PRD_USR}:${process.env.RABBITMQ_PRD_PSS}@${process.env.RABBITMQ_PRD_URI}:${process.env.RABBITMQ_PRD_PORT}`;
+export const amqpDevServer = `amqp://${process.env.RABBITMQ_DEV_USR}:${process.env.RABBITMQ_DEV_PSS}@${process.env.RABBITMQ_DEV_HOST}:${process.env.RABBITMQ_DEV_PORT}`;
+export const amqpPrdServer = `amqp://${process.env.RABBITMQ_PRD_USR}:${process.env.RABBITMQ_PRD_PSS}@${process.env.RABBITMQ_PRD_HOST}:${process.env.RABBITMQ_PRD_PORT}`;
