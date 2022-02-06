ed96d78a662b3b0e9e992f03985ad745666c21f9
Lucas Deschamps <l-deschamps@outlook.com>
2022-02-05 21:25:19 -0300

    wrapping webhook route in try-catch block

--- src/index.ts
+++ src/index.ts
@@ -10,13 +10,17 @@ app.use(express.json());
 const port = process.env.PORT || 3333;
 
 app.post('/', async (req: Request, res: Response): Promise<void> => {
-  publishToQueue(req.body);
+  try {
+    publishToQueue(req.body);
 
-  console.log('request body:', req.body, '\n\n');
+    console.log('INCOMING REQUEST:', req.body, '\n');
 
-  console.log('\nRequest finished.');
-  
-  res.status(200).json({ status: 'success', statusCode: 200 });
+    console.log('\nRequest finished.\n');
+
+    res.status(200).json({ status: 'success', statusCode: 200 });
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 app.listen(port, () => {
